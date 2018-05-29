664db39b32b34327efe5c9804c87bb36e14d1553
kanauchi <kanauchi@tsd-201601-0009>
2018-05-29 14:19:10 +0900

    todoテーブル作成

---
+++ sql/todo.sql
@@ -0,0 +1,12 @@
+CREATE TABLE todo(
+id int AUTO_INCREMENT not null PRIMARY KEY ,
+title varchar(100) not null,
+note varchar(300) not null,
+star varchar(10) not null,
+deadline date
+);
+
+insert into todo values(1, 'java', 'java silver練習', '★★★', '2015/06/15');
+insert into todo values(2, 'html', 'モックアップ作成', '★', null);
+insert into todo values(3, 'database', '副問い合わせ練習', '★★', '2018/05/15');
+insert into todo values(4, 'jsp/サーブレット', '連携を練習', '★★★', '2015/06/15');
